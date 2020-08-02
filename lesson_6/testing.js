e6dfd92738bdca13fd6b02327f1ee2b351d56ad2
Marcus Kwon <Mayaganelou@poets.com>
2020-08-02 03:55:03 +0000

    message

---
+++ lesson_6/testing.js
@@ -0,0 +1,12 @@
+let obj = {
+  'player': 0,
+  'dealer': 0
+};
+
+function display(score) {
+console.log('====== SCORE BOARD ======');
+console.log(score);
+console.log('=========================');
+}
+
+display(obj);
--- lesson_6/twentyone.js
+++ lesson_6/twentyone.js
@@ -1,9 +1,11 @@
 const readline = require('readline-sync');
 const CARDS_SHAPE = ['Heart', 'Diamond', 'Club', 'Spade'];
-const TWENTYONE = 21;
-const SEVENTEEN = 17;
-const HDCSA_VALUES = 10;
+const MAX_NUMBER = 21;
+const DEALER_MIN = 17;
+const HDCS_VALUES = 10;
+const ACE_VALUE_ELEVEN = 11;
 const ACE_VALUE_ONE = 1;
+const WINNING_SCORE = 5;
 const CARDS_VALUE = [
   '2', '3', '4', '5', '6', '7', '8', '9', '10',
   'Ace', 'Jack', 'Queen', 'King'];
@@ -67,7 +69,7 @@ function getUserAnswer(msg, errorMsg, validArray) {
 
 
 function isBusted(array) {
-  return calculatingCards(array) > TWENTYONE;
+  return calculatingCards(array) > MAX_NUMBER;
 }
 
 function nextStep(msg) {
@@ -76,82 +78,116 @@ function nextStep(msg) {
 }
 
 function biggerThanSeventeen(array) {
-  return calculatingCards(array) >= SEVENTEEN;
+  return calculatingCards(array) >= DEALER_MIN;
 }
 
 function detectWinner(playerNumber, dealerNumber) {
-  if ((TWENTYONE - playerNumber) < (TWENTYONE - dealerNumber)) {
+  if ((MAX_NUMBER - playerNumber) < (MAX_NUMBER - dealerNumber)) {
     return 'player';
-  } else if (((TWENTYONE - playerNumber) > (TWENTYONE - dealerNumber))) {
+  } else if (((MAX_NUMBER - playerNumber) > (MAX_NUMBER - dealerNumber))) {
     return 'dealer';
   } else {
     return null;
   }
 }
 
-function displayWinner(playerNumber, dealerNumber) {
-  if (detectWinner(playerNumber, dealerNumber)) {
-    prompt(`The winner is ${detectWinner(playerNumber, dealerNumber)}`);
+function displayWinner(whoWon) {
+  if (whoWon) {
+    prompt(`The winner is ${whoWon}`);
   } else {
     prompt('It is a tie!');
   }
 }
 
 function decidingAceValue(arr, total) {
+  let aceValTotal = 0;
   for (let index = 0; index < arr.length; index++) {
     if (arr[index][VALUE_INDEX] === 'Ace') {
-      if (total + HDCSA_VALUES < TWENTYONE) {
-        return HDCSA_VALUES;
+      if (total + ACE_VALUE_ELEVEN < MAX_NUMBER) {
+        aceValTotal += ACE_VALUE_ELEVEN;
       } else {
-        return ACE_VALUE_ONE;
+        aceValTotal += ACE_VALUE_ONE;
       }
-    } else {
-      return 0;
+
     }
   }
+  return aceValTotal;
 }
 
 function calculatingCards(arr) {
   let total = 0;
+  
   for (let index = 0; index < arr.length; index++) {
     if (!isNaN(Number(arr[index][VALUE_INDEX]))) {
       total += Number(arr[index][VALUE_INDEX]);
     }
-
     if (isNaN(Number(arr[index][VALUE_INDEX])) &&
       arr[index][VALUE_INDEX] !== 'Ace') {
-      total += HDCSA_VALUES;
+      total += HDCS_VALUES;
     }
-
     if (arr[index][VALUE_INDEX] === 'Ace') {
       continue;
     }
   }
-  console.log(decidingAceValue(arr, total))
+  
+  total += decidingAceValue(arr, total);
+  return total;
+}
 
-
-  return (total + decidingAceValue(arr, total));
+ 
+function updateScore(winner, score) {
+  score[winner]++;
 }
 
 
-function keepPlaying(answer) {
-  return !!((answer === 'y' || answer === 'yes'));
+function displayScore(score) {
+  console.log(score);
 }
+
+function whoIsGrandWinner(score) {
+  return Object.keys(score).find(key => score[key] === WINNING_SCORE);
+}
+
+function isWonMatch(score) {
+  return !!whoIsGrandWinner(score);
+}
+
+function displayGrandWinner(score) {
+  if(whoIsGrandWinner(score)) {
+    console.log(`The Grand Winner is: ${whoIsGrandWinner(score)}!!`);
+}
+}
+
+
+
+function isDone(answer) {
+  return !((answer === 'y' || answer === 'yes'));
+}
+
+
+
 
 while (true) {
 
+  nextStep(`WELCOME TO ${MAX_NUMBER}! who reaches ${WINNING_SCORE} points first will be The Grand Winner!`);
+  
+    let scoreBoard = {
+    'dealer': 0,
+    'player': 0
+  };
+  
+while (true) {
+    
   console.clear();
 
   while (true) {
-
-    prompt('WELCOME TO 21!');
 
     let mainDeck = initializingCards();
     shuffle(mainDeck);
 
     let playerCards = [];
     let dealerCards = [];
-
+   
     // first deal to player
     dealingCards(mainDeck, playerCards);
     displayCards('player', playerCards);
@@ -186,32 +222,55 @@ while (true) {
 
     if (isBusted(playerCards)) {
       prompt('Player busted! Dealer won!');
+      updateScore('dealer', scoreBoard);
       break;
     }
+    
+  displayCards('dealer', dealerCards); //showing delears two cards
+
 
     while (true) { //dealer turn
 
-      displayCards('dealer', dealerCards);
-      nextStep('Type anything to move next');
-
       if (biggerThanSeventeen(dealerCards) || isBusted(dealerCards)) break;
-
+      nextStep('Type anything to move next');
       dealingCards(mainDeck, dealerCards);
+      displayCards('dealer', dealerCards);
+
     }
 
     if (isBusted(dealerCards)) {
       prompt('Dealer busted! Player Won!');
+      updateScore('player', scoreBoard);
       break;
     }
-
-    detectWinner(calculatingCards(playerCards), calculatingCards(dealerCards));
-
-    displayWinner(calculatingCards(playerCards), calculatingCards(dealerCards));
+    
+    //nobody busted. now need to compare
+    let winner = detectWinner(calculatingCards(playerCards), calculatingCards(dealerCards));
+    
+    displayWinner(winner);
+    
+    if(winner) {
+    updateScore(winner, scoreBoard);
+    }
+    
     break;
 
   }
+ 
+ displayScore(scoreBoard);
+ 
+ displayGrandWinner(scoreBoard);
+ 
+ nextStep('Please check the score. Type anything to move next');
+ 
+ if(isWonMatch(scoreBoard)) break;
+ 
+ 
+}
 
-  keepPlaying(getUserAnswer('Do you want to play more?',
+let playAgainAnswer = getUserAnswer('Do you want to play more?',
     'Invalid, choose "y" or "n"',
-    VALID_YES_OR_NO));
+    VALID_YES_OR_NO);
+
+  if (isDone(playAgainAnswer)) break;
 }
